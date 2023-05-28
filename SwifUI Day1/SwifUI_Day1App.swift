c03057dcdc553feb1ae3f0e7cb019b1fd4572898
Haidy <haidyyassin16@gmail.com>
2023-05-28 11:24:20 +0300

    Initial Commit

---
+++ SwifUI Day1/SwifUI_Day1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUI_Day1App.swift
+//  SwifUI Day1
+//
+//  Created by Mac on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwifUI_Day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
